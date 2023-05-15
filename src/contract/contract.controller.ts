2fceb13ba35d03ceab3f2be7e7c113ff86c73d02
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-05-15 14:15:02 +0300

    Update contract.controller.ts

--- src/contract/contract.controller.ts
+++ src/contract/contract.controller.ts
@@ -1,47 +1,65 @@
 import { Contract } from '@lib/models';
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 import { ContractService } from './contract.service';
 import { CreateContractDto } from './dto/create-contract.dto';
 import { UpdateContractDto } from './dto/update-contract.dto';
-import { IUpdateWorkers } from './interfaces/updateWorkers.interface';
 
 @Controller('contracts')
 export class ContractController {
   	constructor(private readonly contractService: ContractService) {}
 
 	@MessagePattern({cmd:'create-contract'})
-	async create(@Payload() createContractDto: CreateContractDto):Promise<Contract> {
-		return this.contractService.create(createContractDto);
+	async create(@Payload() createContractDto: CreateContractDto, @Ctx() context: RmqContext):Promise<Contract> {
+		const contract = this.contractService.create(createContractDto);
+		this.actMessage(context)
+		return contract
 	}
 
 	@MessagePattern({cmd:'find-all-contract'})
-	async findAll():Promise<Contract[]> {
-		return this.contractService.findAll();
+	async findAll(@Ctx() context: RmqContext):Promise<Contract[]> {
+		const contracts = this.contractService.findAll();
+		this.actMessage(context)
+		return contracts
 	}
 
 	@MessagePattern({cmd:'find-one-contract'})
-	async findOne(@Payload() id: number):Promise<Contract> {
-		return this.contractService.findOne(id);
+	async findOne(@Payload() id: number, @Ctx() context: RmqContext):Promise<Contract> {
+		const contract = this.contractService.findOne(id);
+		this.actMessage(context)
+		return contract
 	}
 
 	@MessagePattern({cmd:'find-all-by-worker-contract'})
-	async findAllByWorker(@Payload() id: number):Promise<Contract[]> {
-		return this.contractService.findByWorkerId(id);
+	async findAllByWorker(@Payload() id: number, @Ctx() context: RmqContext):Promise<Contract[]> {
+		const contracts = this.contractService.findByWorkerId(id);
+		this.actMessage(context)
+		return contracts
 	}
 
 	@MessagePattern({cmd:'find-all-by-employer-contract'})
-	async findAllByEmployer(@Payload() id: number):Promise<Contract[]> {
-		return this.contractService.findByEmployerId(id);
+	async findAllByEmployer(@Payload() id: number, @Ctx() context: RmqContext):Promise<Contract[]> {
+		const contracts = this.contractService.findByEmployerId(id);
+		this.actMessage(context)
+		return contracts
 	}
 
 	@MessagePattern({cmd:'update-contract'})
-	async update(@Payload() updateContractDto: UpdateContractDto) {
-		return this.contractService.update(updateContractDto.id, updateContractDto);
+	async update(@Payload() updateContractDto: UpdateContractDto, @Ctx() context: RmqContext) {
+		const contract =  this.contractService.update(updateContractDto.id, updateContractDto);
+		this.actMessage(context)
+		return contract
 	}
 
 	@MessagePattern({cmd:'remove-contract'})
-	async delete(@Payload() id: number):Promise<void> {
+	async delete(@Payload() id: number, @Ctx() context: RmqContext):Promise<void> {
+		this.actMessage(context)
 		return this.contractService.delete(id);
+	}
+
+	private actMessage(context: RmqContext){
+		const channel = context.getChannelRef();
+  		const originalMsg = context.getMessage();
+  		channel.ack(originalMsg);
 	}
 }
